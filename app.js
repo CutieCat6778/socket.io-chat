369d0546580016c82f49acf7473d9f90562aef5f
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2020-12-06 11:29:10 +0100

    just setup the socket_io

--- app.js
+++ app.js
@@ -1,38 +1,71 @@
-const createError = require('http-errors');
-const express = require('express');
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socket_io = require('socket.io');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
 const indexRouter = require('./routes/root');
+const createError = require('http-errors');
+require('dotenv').config();
 
-const app = express();
+const port = process.env.port || 3000;
+const app = new express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
 app.use('/', indexRouter);
+
+const server = http.createServer(app);
+const io = socket_io(server);
+
+io.on('connection', (socket) => {
+	console.log(`New user just connected`);
+	
+	socket.emit('newMessage', {
+		from: "Admin",
+		content: 'Wellcome to the chat',
+		time: new Date()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: "Admin",
+		content: 'New user just joined the chat!',
+		time: new Date()
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log(message);
+		io.emit('newMessage', {
+			author: message.author,
+			content: message.content,
+			time: message.time
+		})
+	})
+
+	socket.on('disconnect', () => {
+		console.log(`A user was disconnected from the server!`);
+	})
+
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error', { error: err.message });
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error', { error: err.message });
 });
 
-module.exports = app;
+server.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
+
---
+++ public/js/socket_io.js
@@ -0,0 +1,13 @@
+const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected to server!');
+})
+
+socket.on('disconnect', () => {
+    console.log('Disconnected to server!');
+})
+
+socket.on('newMessage', (message) => {
+    console.log(message);
+})
